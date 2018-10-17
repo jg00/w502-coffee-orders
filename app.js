f04cddd4ca7d360c8b0e1efa82e99c3233894cb9
Jack Gamboa <jgamboa929@gmail.com>
2018-10-16 20:38:01 -0500

    Updates include add and delete order

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const COFFEE_ORDERS = "http://dc-coffeerun.herokuapp.com/api/coffeeorders/"
 
+
 $("#btnAll").click(function(){
     getAllOrders()
 });
+
 
 function getAllOrders() {
 
@@ -23,13 +25,31 @@ function getAllOrders() {
     
                 displayItem = `<div id='divDisplayItem'>
                                     <div class='divDisplayItemElement'>Id: ${id}</div>
-                                    <div class='divDisplayItemElement'>Email: ${emailAddress}</div>
+                                    <div class='divDisplayItemElement'>Email: <span class='emailAddress'>${emailAddress}</span></div>
                                     <div class='divDisplayItemElement'>Order: ${coffee}</div>
+                                    <div class='divDisplayItemElement'>
+                                        <button id='btnRemoveOrder' onclick='RemoveOrder(this)'>Remove Order</button>
+                                    </div>
                               </div>`
     
                 $("#divItems").append(displayItem);
             }
         });
+}
+
+
+function RemoveOrder(sender) {
+
+    let emailToRemove = $(sender).parent().parent().find('.emailAddress').text(); 
+
+    let url = `http://dc-coffeerun.herokuapp.com/api/coffeeorders/${emailToRemove}`;
+    
+    fetch(url, {
+        method: 'delete'
+      }); 
+
+    // Remove from list
+    $(sender).parent().parent().remove();
 }
 
 
@@ -45,7 +65,7 @@ function getOrderByEmail(urlEmail) {
 
     $("#divItems").empty();
 
-    // Fetch coffee order
+    // Fetch coffee orders
     fetch(urlEmail)
         .then(function(response){
             return response.json() 
@@ -58,13 +78,59 @@ function getOrderByEmail(urlEmail) {
 
             displayItem = `<div id='divDisplayItem'>
                                 <div class='divDisplayItemElement'>Id: ${id}</div>
-                                <div class='divDisplayItemElement'>Email: ${emailAddress}</div>
+                                <div class='divDisplayItemElement'>Email: <span class='emailAddress'>${emailAddress}</span></div>
                                 <div class='divDisplayItemElement'>Order: ${coffee}</div>
+
+                                <div class='divDisplayItemElement'>
+                                    <button id='btnRemoveOrder' onclick='RemoveOrder(this)'>Remove Order</button>
+                                </div>
+
                             </div>`
     
                 $("#divItems").append(displayItem);
         });
 }
+
+
+$("#btnAddOrder").click(function(){
+    let txtEmailAddress = $("#txtEmailAddress").val();
+    let txtOrder = $("#txtOrder").val();
+
+    let url = `http://dc-coffeerun.herokuapp.com/api/coffeeorders/`;
+
+    newOrder = { 
+        emailAddress: txtEmailAddress,
+        coffee: txtOrder,
+    }
+
+    createNewOrder(url, newOrder)
+
+    $("#txtEmailAddress").val("");
+    $("#txtOrder").val("");
+});
+
+
+function createNewOrder(postUrl, newOrder) {
+
+    fetch(postUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(newOrder)
+      }) 
+
+}
+
+
+
+
+
+
+
+
+
+
 
 
 
